e8f9fb110e7df2bf79a2e122c2115e901d5f3409
claracoo <claracoo@buffalo.edu>
2020-10-21 01:15:27 -0400

    Task #58 timer is able to bbe toggled for the next round, relevant to USer Story #18

--- doodle.bob/src/components/Clock.jsx
+++ doodle.bob/src/components/Clock.jsx
@@ -7,40 +7,47 @@ const DEFAULT_TIMER = 2;
 
 class Clock extends Component {
 
-    constructor(props) {
-      super(props);
+	constructor(props) {
+		super(props);
 
-        
-      this.state = {
-        seconds: DEFAULT_TIMER,
-        paused: false,
-        status: "Pause",
-		round : ROUND_VALUE,
-		timervalue : DEFAULT_TIMER,
-		maxrounds : 3
-		
-      };
-      this.clockToggle = this.clockToggle.bind(this);
-	  this.setTimer = this.setTimer.bind(this);
-	  this.setRounds = this.setRounds.bind(this);
-    }
-    
-    
-    componentDidMount() {
-        this.timerId = setInterval(
-            () => this.tick(), 1000
-        );
-    }
 
-    componentWillUnmount() {
-        clearInterval(this.timerId);
-    }
+		this.state = {
+			seconds: DEFAULT_TIMER,
+			paused: false,
+			status: "Pause",
+			round: ROUND_VALUE,
+			timervalue: DEFAULT_TIMER,
+			maxrounds: 3
+
+		};
+		this.clockToggle = this.clockToggle.bind(this);
+		this.setTimer = this.setTimer.bind(this);
+		this.setRounds = this.setRounds.bind(this);
+	}
+
+
+	componentDidMount() {
+		this.timerId = setInterval(
+			() => this.tick(), 1000
+		);
+		var newTimer = parseInt(sessionStorage.getItem("timeAmount"));
+
+		this.setState({
+			timervalue: newTimer,
+			seconds: newTimer
+		});
+		console.log(newTimer)
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timerId);
+	}
 
 	setRounds(obj) {
 		var newMaxRound = parseInt(obj.target.value);
 
 		this.setState({
-			maxrounds : newMaxRound
+			maxrounds: newMaxRound
 		});
 	}
 
@@ -48,76 +55,77 @@ class Clock extends Component {
 		var newTimer = parseInt(obj.target.value);
 
 		this.setState({
-			timervalue : newTimer
+			timervalue: newTimer
 		});
+		sessionStorage.setItem("timeAmount", obj.target.value)
 	}
 
-    clockToggle() {
-		if(this.state.status != "Game Over")
-		{
+	clockToggle() {
+		if (this.state.status != "Game Over") {
 			this.setState({
-			paused: !this.state.paused,
-			status: this.state.paused?"Pause":"Resume"
+				paused: !this.state.paused,
+				status: this.state.paused ? "Pause" : "Resume"
 			});
 		}
-      if(this.state.status == "Start Next Round") {
-        this.setState({
-		  seconds : this.state.timervalue,
-          paused: false,
-          status: "Pause"
-        });
-		sessionStorage.setItem("preRound", true)
-		window.location.reload(false);
-      }
-    }
+		if (this.state.status == "Start Next Round") {
+			this.setState({
+				seconds: this.state.timervalue,
+				paused: false,
+				status: "Pause"
+			});
+			sessionStorage.setItem("preRound", true)
+			window.location.reload(false);
+		}
+	}
 
-    tick() {
-      if(this.state.seconds <= 1 && this.state.status != "Game Over") {
-	clearInterval(this.timerId);
-        this.setState({
-        status: "Start Next Round",
-	round : this.state.round + 1
-        })
-      }
-  
-	  if(this.state.round > this.state.maxrounds) {
-	  this.setState({
-		status : "Game Over",
-		seconds : 0,
-		round : "has ended"
+	tick() {
+		if (this.state.seconds <= 1 && this.state.status != "Game Over") {
+			clearInterval(this.timerId);
+			this.setState({
+				status: "Start Next Round",
+				round: this.state.round + 1
+			})
+		}
 
-	  });
-	  }
-      if(this.state.paused == false && this.state.status != "Game Over") {
-        this.setState({
-            seconds: this.state.seconds - 1
-        });
-    
-      }   
-    }
+		if (this.state.round > this.state.maxrounds) {
+			this.setState({
+				status: "Game Over",
+				seconds: 0,
+				round: "has ended"
 
-    render(props) {
-        return (
-		<div>
+			});
+		}
+		if (this.state.paused == false && this.state.status != "Game Over") {
+			this.setState({
+				seconds: this.state.seconds - 1
+			});
+
+		}
+	}
+
+	render(props) {
+		return (
 			<div>
-				<button value = "5" onClick={this.setRounds}>5 Rounds</button>
-				<button value = "10" onClick={this.setRounds}>10 Rounds</button>
-				<button value = "15" onClick={this.setRounds}>15 Rounds</button>
-				
+				<div>
+					<button value="5" onClick={this.setRounds}>5 Rounds</button>
+					<button value="10" onClick={this.setRounds}>10 Rounds</button>
+					<button value="15" onClick={this.setRounds}>15 Rounds</button>
+
+				</div>
+				<div>
+					<button value="2" onClick={this.setTimer}>2 seconds</button>
+					<button value="30" onClick={this.setTimer}>30 seconds</button>
+					<button value="60" onClick={this.setTimer}>60 seconds</button>
+					<button value="90" onClick={this.setTimer}>90 seconds</button>
+				</div>
+				<div id="clock">
+					<p>{this.state.seconds + "s"}</p>
+					<button onClick={this.clockToggle}>{this.state.status}</button>
+					<p>{"Round " + this.state.round}</p>
+				</div>
 			</div>
-			<div>
-				<button value = "30" onClick={this.setTimer}>30 seconds</button>
-				<button value = "60" onClick={this.setTimer}>60 seconds</button>
-				<button value = "90" onClick={this.setTimer}>90 seconds</button>
-			</div>
-	     <div id="clock">
-	         <p>{this.state.seconds + "s"}</p>
-	         <button onClick={this.clockToggle}>{this.state.status}</button>
-		    <p>{"Round " + this.state.round}</p>
-	     </div>
-		</div>
-    );
-    }
+		);
+	}
 }
 
 export default Clock;
